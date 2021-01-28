a3e82531d5c1c05aef356a439a627525e756d2b6
“devidas” <devidas@platform9.com>
2021-01-28 19:03:14 +0530

    added function to do task

--- calc.go
+++ calc.go
@@ -4,7 +4,15 @@ import "fmt"
 
 func main()  {
 	val1,val2 := 2 , 3
-	addition := val1+val2
-	multiplication := val1*val2
-	fmt.Println(addition,multiplication)
+	fmt.Println(Addition(val1,val2))
+	fmt.Println(Multiplication(val1,val2))
+	
+}
+
+func Addition(val1, val2 int) int  {
+	return val1 + val2
+}
+
+func Multiplication(val1, val2 int) int {
+	return val1 * val2
 }
